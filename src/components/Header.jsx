10719c23e21511a346a1fe9b9487711c8e151b6b
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-03-30 00:09:41 +0600

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css'
+import logo from "../../src/images/Logo.svg";
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar text-white ema-jon-nav-bar">
+              <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">
+                    <img src={logo} />
+                </a>
+              </div>
+              <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                  <li><a href='/#'>Order</a></li>
+                  <li><a href='/#'>Order Review</a></li>
+                  <li><a href='/#'>Manage Inventory</a></li>
+                  <li><a href='/#'>Login</a></li>
+                </ul>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#46db61",
+        
+"secondary": "#3888a5",
+        
+"accent": "#e2739a",
+        
+"neutral": "#311F33",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#6885DF",
+        
+"success": "#2DC868",
+        
+"warning": "#FBB946",
+        
+"error": "#F17495",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
