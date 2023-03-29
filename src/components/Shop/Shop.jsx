3c02d17d6f76313d4ed7bdc0093f3422c2ddb84b
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-03-30 00:41:00 +0600

    create header components and cut header.jsx and header.css file and paste header component

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Shop;
