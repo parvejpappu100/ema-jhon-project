d02ed65492743124ceb6e7ad427333f7ba9d0f7a
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-04-08 02:25:05 +0600

    added navigation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
+            <div className='lg:container mx-auto text-2xl text-center my-5'>{navigation.state === "loading" && "Loading...."}</div>
             <Outlet></Outlet>
         </div>
     );
